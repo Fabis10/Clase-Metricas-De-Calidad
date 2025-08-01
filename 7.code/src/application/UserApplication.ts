94d6b2562489590e950282168cf520257bf0b40f
DilanBernal <dilanbernal01@gmail.com>
2025-08-01 15:42:37 -0500

    clase 01/08/2025

---
+++ 7.code/src/application/UserApplication.ts
@@ -0,0 +1,45 @@
+import User from "../domain/User";
+import UserPort from "../domain/UserPort";
+
+export default class UserApplication {
+
+  private port: UserPort;
+
+  constructor(port: UserPort) {
+    this.port = port;
+  }
+
+  async createUser(user: Omit<User, "id">): Promise<number> {
+    const existingUser = await this.port.getUserByEmail(user.email);
+
+    if (!existingUser) {
+      return await this.port.createUser(user);
+    }
+    throw new Error("El usuario ya existe");
+  }
+
+  async updateUser(id: number, user: Partial<User>): Promise<boolean> {
+    const existingUser = await this.port.getUserById(id);
+
+    if (!existingUser) {
+      throw new Error("El usuario no existe");
+    }
+    if (user.email) {
+      const emailTaken = await this.port.getUserByEmail(user.email);
+      if (emailTaken && emailTaken.id !== id) {
+        throw new Error("No se puede cambiar el correo porque alguien ya lo esta usando");
+      }
+    }
+    return await this.port.updateUser(id, user);
+  }
+
+  async deleteUser(id: number): Promise<boolean> {
+    const existingUser = await this.port.getUserById(id);
+
+    if (!existingUser) {
+      throw new Error("dsfsdf");
+    }
+
+    return await this.port.deleteUser(id);
+  }
+}
---
+++ 7.code/src/infraestructure/config/con_data_base.ts
@@ -0,0 +1,29 @@
+
+import dotenv from "dotenv";
+import { DataSource } from "typeorm";
+import UserEntity from "../entities/UserEntity";
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: "mysql",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  logging: true,
+  entities: [
+    UserEntity,
+  ]
+});
+
+export const connectDB = async () => {
+  try {
+    await AppDataSource.initialize();
+    console.log("Database connected");
+  } catch (error) {
+    console.error("Error connecting to the DB", error);
+    process.exit(1);
+  }
+}
